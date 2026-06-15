76def7c4b7555541b52cfdd70c76fb2a6e7dc322
LucioFss <fassanolucio06@gmail.com>
2026-06-15 11:22:43 -0300

    Se agregan todos los archivos del proyecto
    El codigo agregado no esta compilado

---
+++ matrices.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+extern void sumar_matrices(int *A, int *B, int *C, int cantElementos);  //Funcion externa de assembler
+int main(){
+    int matriz1[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    int matriz2[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+    int matriz3[4][4] = {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+    
+    
+    printf("Sobreviví al Assembler!\n");
+
+    for(int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%d ", matriz3[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
